9db5b71dfd0469a63ab0b9e34f5884ad39af2ef7
eyadmohamedelzeky <eyadmohamedelzeky@gmail.com>
2022-12-09 17:21:52 +0200

    first commit

---
+++ lib/models/product_model.dart
@@ -0,0 +1,40 @@
+class ProductModel {
+  final dynamic id;
+  final String title;
+  final dynamic price;
+  final String description;
+  final String image;
+  final dynamic category;
+  final RatingModel rating;
+
+  ProductModel({
+    required this.category,
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.image,
+    required this.rating,
+  });
+
+  factory ProductModel.fromjson(jsonData) {
+    return ProductModel(
+      category: jsonData['category'],
+      id: jsonData['id'],
+      title: jsonData['title'],
+      price: jsonData['price'],
+      description: jsonData['description'],
+      image: jsonData['image'],
+      rating: RatingModel.fromjson(jsonData['rating']),
+    );
+  }
+}
+
+class RatingModel {
+  final dynamic rate;
+  final int count;
+  RatingModel({required this.rate, required this.count});
+  factory RatingModel.fromjson(jsonData) {
+    return RatingModel(rate: jsonData['rate'], count: jsonData['count']);
+  }
+}
